ed035cee2faf21921e020018bc23bf8ca94eb1ac
Chad Hinds <chadrickj8@gmail.com>
2021-09-29 17:45:25 -0400

    Initial commit before initial push

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import { Container } from './components/styles/Container.styled'
+function App() {
+  return (
+    <Container className="App">
+
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/components/styles/Container.styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+// inside the string template is where you put your css
+
+export const Container = styled.div`
+    width: 1000px;
+    max-width: 100%;
+    padding: 0 20px;
+    margin: 0 auto;
+`
