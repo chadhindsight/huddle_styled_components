7911c7a24c226904b6beee39c49fe0d89ec92bd9
Chad Hinds <chadrickj8@gmail.com>
2021-09-30 20:25:47 -0400

    Use a theme provider

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import { Container } from './components/styles/Container.styled'
+import { ThemeProvider } from 'styled-components';
+import { Header } from './components/Header';
+import { Container } from './components/styles/Container.styled';
+
+// Use a theme provider
+const theme = {
+  colors: {
+    header: '#ebfbff',
+    body: '#fff',
+    footer: '#003333',
+  },
+}
 function App() {
   return (
-    <Container className="App">
-
-    </Container>
+    <ThemeProvider>
+      <>
+        <Header />
+        <Container className="App">
+          <h1>Hello World</h1>
+        </Container>
+      </>
+    </ThemeProvider>
   );
 }
 
