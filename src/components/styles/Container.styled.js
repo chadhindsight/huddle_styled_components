6e4700974fe39d229a99b7fb825c671c44557ae8
chad hinds <chadrickj8@gmail.com>
2022-03-28 12:26:37 -0400

    Explain use of styled components

--- src/components/styles/Container.styled.js
+++ src/components/styles/Container.styled.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-//inside the string template is where you put your css.
+//inside the string template is where you put your css
 export const Container = styled.div`
     width: 1000px;
     max-width: 100%;
