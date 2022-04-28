ebcc50f5903e80c9599a9e7d9247d3e7e816cb4c
chad hinds <chadrickj8@gmail.com>
2022-04-28 14:47:12 -0400

    adjust max width

--- src/components/styles/Container.styled.js
+++ src/components/styles/Container.styled.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 //inside the string template is where you put your css
 export const Container = styled.div`
     width: 1000px;
-    max-width: 100%;
+    max-width: 90%;
     padding: 0 21px;
     margin: 0 auto;
 `
